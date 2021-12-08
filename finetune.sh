0d84ce29f695edd1d51392fc2d08f21b5412e45c
Anant Kharkar <kharkarag@gmail.com>
2021-12-08 23:00:52 +0000

    Added generic finetuning script

---
+++ finetune.sh
@@ -0,0 +1,26 @@
+#!/bin.bash
+
+# Models:  
+# BERT (bert-base-uncased)
+# BERT-small (prajjwal1/bert-small)
+# BERT-tiny (prajjwal1/bert-tiny)
+# DistilBERT (distilbert-base-uncased)
+#
+# Tasks:
+# sst2
+#
+# Example usage: bash finetune.sh bert-base-uncased sst2
+# Example output dir: models/bert-base-uncased_sst2
+
+python3 run_glue.py \
+    --model_name_or_path $1 \
+    --task_name $2 \
+    --output_dir models/$1_$2/ \
+    --per_gpu_train_batch_size 64 \
+    --per_gpu_eval_batch_size 64 \
+    --learning_rate ... \
+    --do_train \
+    --do_eval \
+    --overwrite_output_dir \
+    --seed 100 \
+    --max_seq_length 128
